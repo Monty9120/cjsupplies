7a585510b03b49dcbfe2c1b8e0d9475449ee6fd4
montague michael llewelyn morphy <845873736@cit3-501m-14.acg.ac.nz>
2018-11-30 15:10:07 +1300

    animations

--- js/script.js
+++ js/script.js
@@ -1,41 +1,74 @@
 $(function(){
 //Small Nav on Scroll
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    $('.nav').removeClass('smallnav')
-  } else {
-    $('.nav').addClass('smallnav')
+if ($(window).width() >= 700) {
+
+  var prevScrollpos = window.pageYOffset;
+  window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      $('.nav').removeClass('smallnav')
+    } else {
+      $('.nav').addClass('smallnav')
+      // $('.nav-content').find('ul').css('opacity','0')
+
+    }
+    prevScrollpos = currentScrollPos;
   }
-  prevScrollpos = currentScrollPos;
+
+
 }
 
+
+
 //Mobile nav
-$('.fa-times').hide();
+$('.fa-times').css('transform','scale(0)')
+
+var isFirst = true;
+
 $('.fa-bars').on('click',function(){
-	$('.nav-content').find('ul').css('transform','translateX(0px)')
+	// $('.nav-content').find('ul').css('transform','translateX(0px)')
 	$('.nav-content').find('ul').css('opacity','1')
-	$('.fa-bars').hide();
-	$('.fa-times').show();
+
+
+	mobileMenu.play();
+  if(isFirst == false){
+    mobileMenu.reverse();
+  }
+  isFirst = false;
+
+
+	$('.fa-bars').css('transform','scale(0)');
+	$('.fa-times').css('transform','scale(1)')
 });
+
+
 $('.fa-times').on('click',function(){
-	$('.nav-content').find('ul').css('transform','translateX(300px)')
-	$('.nav-content').find('ul').css('opacity','0')
-	$('.fa-times').hide();
-	$('.fa-bars').show();
+
+	mobileMenu.play();
+  mobileMenu.reverse();
+	$('.fa-times').css('transform','scale(0)');
+	$('.fa-bars').css('transform','scale(1)');
+
+	
 });
 
 
-//Title animation
-var title = anime({
-  targets: '.title',
-  delay: 1000,
-  translateY: 200,
-  opacity:1,
-  duration:1000,
+
+var mobileMenu = anime({
+  targets: '.nav-items li',
+  translateX: '-100%',
+  delay: function(el, i, l) { return i * 100; },
+  // direction: 'alternate',
+  // delay:200,
+  // loop: true,
+  autoplay:false,
+  easing: 'easeOutExpo',
+
 
 });
+
+
+
 
 });
 
